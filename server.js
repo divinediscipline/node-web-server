e49348d4fced4d65ede6a5535cdf29f9bdc9c730
Divinelove <divineloves77@gmail.com>
2017-10-25 20:59:35 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const hbs = require("hbs");
+
+const app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+
+app.use((req, res, next) => {
+    res.render("maintenance.hbs");
+    
+});
+
+app.use(express.static(__dirname + "/public"));
+
+console.log(new Date().toString());
+hbs.registerHelper("getCurrentYear", () => {
+   return new Date().getFullYear(); 
+});
+
+
+app.get("/", (req, res) => {
+    res.render("home.hbs", {
+        pageTitle: "Home",
+        welcomeMessage: "Hey there! am using Nodejs."
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about.hbs", {
+        pageTitle: "About Page" 
+    });
+});
+
+app.get("/bad", (req, res) => {
+    res.send({
+        errorMessage: "bad message"
+    });
+});
+
+app.listen(3000,() =>{
+    console.log("App is running on port 3000!")
+});
+
